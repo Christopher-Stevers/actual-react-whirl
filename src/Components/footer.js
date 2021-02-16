318ed8746f9de8b68e2d8f086281a729c90a836b
Christopher Stevers <christopher.stevers1@gmail>
2021-02-16 18:33:53 -0500

    fixed mobile overlap in contact page

--- src/Components/footer.js
+++ src/Components/footer.js
@@ -9,7 +9,7 @@ function Footer() {
         <Link  to="/">Home</Link>
         <Link  to="/contact">Contact</Link>
         <Link to="/shop" >Shop</Link>
-        <div className={style.referal}>Designed by <a href='http://www.devstevers.tk/' className='footer'>CStevers Development</a></div></footer>
+        <div className={style.referal}>Designed by <a href='http://www.devstevers.tk/' className='footer'>CStevers Development</a>     <div>Icons made by <a href="https://www.freepik.com" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div></div></footer>
         
       </div>
     );
