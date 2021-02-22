7cb786dfeecbb5352eae770731896d1714fb19db
Christopher Stevers <christopher.stevers1@gmail>
2021-02-22 13:28:51 -0500

    updated path for cow image

--- src/Components/body.js
+++ src/Components/body.js
@@ -1,4 +1,5 @@
 import style from './body.module.scss';
+import bodyImg from '../bodyImg.webp'
 const text=`Whirl Creek Farm is an organic family farm in Perth County, Ontario's agricultural heartland. We started working toward chemical free farming  nearly 50 years ago. Now we provide premium meats such as certified organic pork and grassfed beef.
 
 We make it our mission to produce high quality meat for you the consumer. Since 1974 we've been fine tuning our production to offer ever-more nutritious and sustainable products. All our animals are raised in humane surroundings with a focus on organic production. Besides growing the best meat we can, we also produce grains for feed and flour. These include spelt, wheat, corn, soy, and barley. While most of this goes to the animals, we do offer whole spelt kernels and wheat for human consumption. If you grind your own flour this is the place for you!
@@ -9,7 +10,7 @@ function Body() {
     return (<div className={style.body}>
         <article><p className={style.p}>
 
-<img className={style.img} src="src\bodyImg.webp" alt='cow'></img>{text}</p></article>
+<img className={style.img} src={bodyImg} alt='cow'></img>{text}</p></article>
         </div>
     );
   }
